cf5fcebce545fa8c137a0c10871e8fddfef31705
Alan Smith <commit@alanwsmith.com>
2023-03-15 00:32:39 -0400

    Setting up a parser example for the sections

---
+++ site/example_code/src/bin/nom_neopolitan_token_parser.rs
@@ -0,0 +1,60 @@
+#![allow(warnings)]
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::bytes::complete::take_until;
+use nom::character::complete::char;
+use nom::character::complete::multispace0;
+use nom::character::complete::multispace1;
+use nom::combinator::eof;
+use nom::combinator::rest;
+use nom::multi::many_till;
+use nom::sequence::delimited;
+use nom::sequence::pair;
+// use nom::sequence::preceded;
+use nom::sequence::tuple;
+use nom::IResult;
+// use nom::Parser;
+
+#[derive(Debug)]
+enum RawBlock {
+    Title { text: String },
+    P { text: String },
+    Error { text: String },
+}
+
+fn main() {
+    let source = "-> TITLE\n\na title\n\n-> P\n\nsome\n\nparagraphs";
+    let parts = split_tokens(source);
+    dbg!(parts);
+}
+
+fn split_tokens(source: &str) -> Vec<RawBlock> {
+    let (_, tokens) = many_till(do_split, eof)(source).unwrap();
+    tokens.0
+}
+
+fn do_split(source: &str) -> IResult<&str, RawBlock> {
+    dbg!(source);
+    let (source, _) = multispace0(source)?;
+    dbg!(source);
+    let (source, _) = tag("-> ")(source)?;
+    let (source, value) = tuple((
+        alt((tag("TITLE"), tag("P"))),
+        tag("\n\n"),
+        alt((take_until("\n\n-> "), rest)),
+    ))(source)?;
+
+    let response = match value.0 {
+        "TITLE" => RawBlock::Title {
+            text: value.2.to_string(),
+        },
+        "P" => RawBlock::P {
+            text: value.2.to_string(),
+        },
+        _ => RawBlock::Error {
+            text: value.2.to_string(),
+        },
+    };
+    // dbg!(source);
+    Ok((source, response))
+}
