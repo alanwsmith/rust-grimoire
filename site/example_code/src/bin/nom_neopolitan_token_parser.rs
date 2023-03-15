13eb605f8f02c03766bd69becfdf9e8acfc56d40
Alan Smith <commit@alanwsmith.com>
2023-03-15 01:56:25 -0400

    Workignon the parser

--- site/example_code/src/bin/nom_neopolitan_token_parser.rs
+++ site/example_code/src/bin/nom_neopolitan_token_parser.rs
@@ -1,22 +1,28 @@
-#![allow(warnings)]
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::bytes::complete::take_until;
-use nom::character::complete::char;
+use nom::character::complete::line_ending;
 use nom::character::complete::multispace0;
-use nom::character::complete::multispace1;
+use nom::character::complete::space0;
 use nom::combinator::eof;
 use nom::combinator::rest;
-use nom::multi::many_till;
-use nom::sequence::delimited;
-use nom::sequence::pair;
-// use nom::sequence::preceded;
-use nom::sequence::tuple;
-use nom::IResult;
-// use nom::Parser;
 use nom::error::Error;
 use nom::error::ErrorKind;
+use nom::multi::many_till;
+use nom::sequence::tuple;
 use nom::Err;
+use nom::IResult;
+
+// Note: this only requires one line to be
+// above the `-> ` token. Or, more to the
+// point it just looks for a newline
+// before it. Ideally, it would be two,
+// but that's more complicated than I
+// want to put the time into for now.
+//
+// Two empty lines are required after
+// a header. They can have whitespace
+// but no other characters.
 
 #[derive(Debug, PartialEq)]
 enum RawBlock {
@@ -30,10 +36,10 @@ fn main() {
     println!("Testing...");
 
     // Basic test
-    let source = "-> TITLE\n\na title\n\n-> P\n\nsome\n\nparagraphs";
+    let source = "-> TITLE\n\nA Title\n\n-> P\n\nsome\n\nparagraphs";
     let expected: Vec<RawBlock> = vec![
         RawBlock::Title {
-            text: "a title".to_string(),
+            text: "A Title".to_string(),
         },
         RawBlock::P {
             text: "some\n\nparagraphs".to_string(),
@@ -42,17 +48,19 @@ fn main() {
     assert_eq!(expected, split_tokens(source).unwrap().1);
 
     // Multiple paragraphs
-    let source = r#"-> CODE
-
+    // White space added after headers too
+    let source = r#"-> CODE  
+   
 some code
 
--> P
-
+-> P   
+   
 para1
 
 -> P
 
 para2"#;
+
     let expected: Vec<RawBlock> = vec![
         RawBlock::Code {
             text: "some code".to_string(),
@@ -66,16 +74,8 @@ para2"#;
     ];
     assert_eq!(expected, split_tokens(source).unwrap().1);
 
+    // Error on invalid headers
     let source = "-> INVALIDHEADER\n\nis invalid";
-    let expected: Vec<RawBlock> = vec![
-        RawBlock::Title {
-            text: "a title".to_string(),
-        },
-        RawBlock::P {
-            text: "some\n\nparagraphs".to_string(),
-        },
-    ];
-
     assert_eq!(
         Err(Err::Error(Error::new(
             "INVALIDHEADER\n\nis invalid",
@@ -83,7 +83,6 @@ para2"#;
         ))),
         split_tokens(source)
     );
-
     println!("Process complete");
 }
 
@@ -92,30 +91,28 @@ fn split_tokens(source: &str) -> IResult<&str, Vec<RawBlock>> {
     Ok(("", tokens.0))
 }
 
+fn newline_pair(source: &str) -> IResult<&str, &str> {
+    let (source, _) = space0(source)?;
+    let (source, _) = line_ending(source)?;
+    let (source, _) = space0(source)?;
+    let (source, _) = line_ending(source)?;
+    Ok((source, value))
+}
+
 fn do_split(source: &str) -> IResult<&str, RawBlock> {
-    //dbg!(source);
     let (source, _) = multispace0(source)?;
-    // dbg!(source);
     let (source, _) = tag("-> ")(source)?;
     let (source, value) = tuple((
         alt((tag("TITLE"), tag("CODE"), tag("P"))),
-        tag("\n\n"),
-        alt((take_until("\n\n-> "), rest)),
+        newline_pair,
+        alt((take_until("\n-> "), rest)),
     ))(source)?;
-
+    let string_value = value.2.trim().to_string();
     let response = match value.0 {
-        "TITLE" => RawBlock::Title {
-            text: value.2.to_string(),
-        },
-        "P" => RawBlock::P {
-            text: value.2.to_string(),
-        },
-        "CODE" => RawBlock::Code {
-            text: value.2.to_string(),
-        },
-        _ => RawBlock::Error {
-            text: value.2.to_string(),
-        },
+        "TITLE" => RawBlock::Title { text: string_value },
+        "P" => RawBlock::P { text: string_value },
+        "CODE" => RawBlock::Code { text: string_value },
+        _ => RawBlock::Error { text: string_value },
     };
     Ok((source, response))
 }
