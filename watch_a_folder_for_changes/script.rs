b90a23a188ec3f082895f5cc3c39b21de019aa0f
Alan Smith <commit@alanwsmith.com>
2025-05-23 14:03:58 -0400

    cleanup

--- watch_a_folder_for_changes/script.rs
+++ watch_a_folder_for_changes/script.rs
@@ -9,23 +9,15 @@ notify-debouncer-full = "0.5.0"
 rusqlite = "0.35.0"
 sha2 = "0.10.9"
 tokio = {version = "1.45.0", features = ["rt-multi-thread", "macros", "sync"] }
-
-[grimoire]
-created = "2025-05-21T17:54:09-04:00"
-updated = "2025-05-21T17:54:09-04:00"
-id = "2x/v4/9l/ge"
-status = "scratch"
 ---
 
-#![allow(unused)]
-use anyhow::Error;
+
+
 use anyhow::Result;
 use itertools::Itertools;
-use notify::Event;
 use notify::EventKind;
 use notify::RecursiveMode;
 use notify_debouncer_full::*;
-use rusqlite::Connection;
 use std::path::PathBuf;
 use std::time::Duration;
 use tokio::runtime::Handle;
@@ -61,7 +53,6 @@ impl DirWatcher {
         tokio::spawn(async move {
             let rt = Handle::current();
             let (internal_tx, mut internal_rx) = mpsc::channel::<Vec<PathBuf>>(2);
-            let internal_tx2 = internal_tx.clone();
             let mut debouncer = new_debouncer(
                 Duration::from_millis(400),
                 None,
@@ -137,76 +128,9 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-// conn: Connection,
-//debouncer: Debouncer<notify::RecommendedWatcher,notify_debouncer_full::RecommendedCache>,
-//rx: Receiver<Result<bool, Error>>,
-//tx: Sender<Result<bool, Error>>,
 
-//let (tx, rx) = mpsc::channel::<Result<bool>>();
-
-//        dw.init_db();
-//
-//let conn = Connection::open_in_memory()?;
-
-// fn init_db(&self)  -> Result<()> {
-//     println!("Creating hash database");
-//     let create_table_sql = "CREATE TABLE IF NOT EXISTS
-//         files (
-//             path TEXT PRIMARY KEY,
-//             hash TEXT NOT NULL
-//         )";
-//     self.conn.execute(create_table_sql, ())?;
-//     Ok(())
-// }
-
-//debouncer.watch(path, RecursiveMode::Recursive)?;
-
-// for res in rx {
-//     dbg!("asdf");
-//     // match res {
-//     //     Ok(event) => println!("event: {:?}", event),
-//     //     Err(e) => println!("watch error: {:?}", e),
-//     // }
-// }
-
-// for res in dw.rx {
-//     dbg!("asdf");
-//     // match res {
-//     //     Ok(event) => println!("event: {:?}", event),
-//     //     Err(e) => println!("watch error: {:?}", e),
-//     // }
-// }
-
-// fn watch_folder(path: &PathBuf) -> Result<()> {
-//     println!("Starting watcher");
-//     let conn = Connection::open_in_memory()?;
-//     init_db(&conn)?;
-//     let (tx, rx) = mpsc::channel::<Result<Vec<DebouncedEvent>, Vec<notify::Error>>>();
-//     let mut debouncer = new_debouncer(
-//         Duration::from_millis(100),
-//         None,
-//         tx
-//     )?;
-//     debouncer.watch(path, RecursiveMode::Recursive)?;
-//     for res in rx {
-//         match res {
-//             Ok(event) => println!("event: {:?}", event),
-//             Err(e) => println!("watch error: {:?}", e),
-//         }
-//     }
-//     Ok(())
-// }
-
-//let tx2 = tx.clone();
-// let mut debouncer = new_debouncer(
-//     Duration::from_millis(100),
-//     None,
-//     move |result: DebounceEventResult| {
-//         dbg!(result);
-//         tx2.send(Ok(true));
-//         // match result {
-//         //     Ok(events) => events.iter().for_each(|event| println!("{event:?}")),
-//         //     Err(errors) => errors.iter().for_each(|error| println!("{error:?}")),
-//         // }
-//     }
-// )?;
+// -- metadata 
+// -- created: 2025-05-21T17:54:09-04:00
+// -- updated: 2025-05-21T17:54:09-04:00
+// -- id: 2x/v4/9l/ge
+// -- status: draft 
