b7a33e92473cf0dabfc12ae92502c3ddd1b6b020
Alan Smith <commit@alanwsmith.com>
2025-05-23 10:06:29 -0400

    moved review files out

--- _to_review/old_example_code/src/bin/ksuid_date_maker.rs
+++
@@ -1,8 +0,0 @@
-use svix_ksuid::*;
-use time::macros::datetime;
-
-fn main() {
-    let raw_ksuid = Ksuid::new(Some(datetime!(2017-11-16 12:12:01 UTC)), None);
-    let ksuid_string = raw_ksuid.to_string();
-    println!("{}", ksuid_string);
-}
