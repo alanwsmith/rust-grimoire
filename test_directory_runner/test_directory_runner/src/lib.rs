e22b86131968b0eccd8e67bb182e45bc3211a5d7
Alan Smith <commit@alanwsmith.com>
2025-06-10 14:56:03 -0400

    Renamed for cleaner separation

--- test_directory_runner/test_directory_runner/src/lib.rs
+++ test_directory_runner/test_directory_runner/src/lib.rs
@@ -6,12 +6,12 @@ pub fn widget_test(input: &str) -> bool {
 mod tests {
   #![allow(unused)]
   use super::*;
+  use macros_for_test_directory_runner::test_dir;
   use pretty_assertions::assert_eq;
-  use test_runner_macros::test_dir;
 
   fn test_router(input: &str) -> bool {
     widget_test(input)
   }
 
-  test_dir!("test_runner/src/test_cases");
+  test_dir!("test_directory_runner/src/test_cases");
 }
