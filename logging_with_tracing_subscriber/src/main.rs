99874d3c46fc20510e3e43024abb59489ea1b07b
Alan Smith <commit@alanwsmith.com>
2025-06-13 18:20:24 -0400

    Getting stdout and stderror

--- logging_with_tracing_subscriber/src/main.rs
+++ logging_with_tracing_subscriber/src/main.rs
@@ -84,6 +84,16 @@ impl Logger {
     }
   }
 
+  pub fn with_stderr(
+    self,
+    level: LevelFilter,
+  ) -> Self {
+    Self {
+      stderr: Some(level),
+      ..self
+    }
+  }
+
   pub fn init(self) -> Self {
     let stdout_layer = match self.stdout {
       Some(level) => {
@@ -104,9 +114,29 @@ impl Logger {
       None => None,
     };
 
+    let stderr_layer = match self.stderr {
+      Some(level) => {
+        let format = tracing_subscriber::fmt::format()
+          .without_time()
+          .with_target(false)
+          .with_thread_ids(false)
+          .with_thread_names(false)
+          .with_ansi(false)
+          .with_line_number(false)
+          .with_file(false);
+        let layer = fmt::Layer::default()
+          .event_format(format)
+          .with_writer(std::io::stderr)
+          .with_filter(level);
+        Some(layer)
+      }
+      None => None,
+    };
+
     let subscriber =
       tracing_subscriber::Registry::default()
-        .with(stdout_layer);
+        .with(stdout_layer)
+        .with(stderr_layer);
 
     tracing::subscriber::set_global_default(subscriber)
       .expect("unable to set global subscriber");
@@ -192,6 +222,7 @@ fn main() {
 
   let logger_guard = Logger::setup()
     .with_stdout(LevelFilter::INFO)
+    .with_stderr(LevelFilter::INFO)
     .init();
 
   // let logger_guard = Logger::setup().init();
