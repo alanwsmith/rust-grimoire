531ccaa4396ae265dc5e67e11dfef61cf86906e7
Alan Smith <commit@alanwsmith.com>
2025-06-13 15:23:37 -0400

    Working on logging

--- logging_with_tracing_subscriber/src/main.rs
+++ logging_with_tracing_subscriber/src/main.rs
@@ -1,30 +1,46 @@
 #![allow(unused)]
-//use tracing::{Level, event, instrument};
-//
-
-use tracing::{Level, event, instrument};
-use tracing_subscriber::fmt;
+use std::path::PathBuf;
+use tracing::{Level, Subscriber, event, instrument};
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::fmt::{self, Layer};
+use tracing_subscriber::layer::Layered;
 use tracing_subscriber::prelude::*;
+
+struct Logger {
+  guard: WorkerGuard,
+}
+
+impl Logger {
+  pub fn new() -> Logger {
+    let log_dir = PathBuf::from("test-output");
+    let log_basename = "example-output.log".to_string();
+    // set_up_logging(&log_dir, &log_basename);
+    let file_appender =
+      tracing_appender::rolling::never(
+        log_dir,
+        log_basename,
+      );
+    let (file_writer, guard) =
+      tracing_appender::non_blocking(file_appender);
+    let file_layer_format =
+      tracing_subscriber::fmt::format().json();
+    let file_layer = fmt::Layer::default()
+      .event_format(file_layer_format)
+      .with_writer(file_writer)
+      .json();
+    let subscriber =
+      tracing_subscriber::Registry::default()
+        .with(file_layer);
+    tracing::subscriber::set_global_default(subscriber)
+      .expect("unable to set global subscriber");
+    let logger = Logger { guard };
+    logger
+  }
+}
 
 #[instrument]
 fn main() {
-  let file_appender = tracing_appender::rolling::never(
-    "test-stuff",
-    "example-output.log",
-  );
-  let (file_writer, _guard) =
-    tracing_appender::non_blocking(file_appender);
-  let file_layer_format =
-    tracing_subscriber::fmt::format().json();
-  let file_layer = fmt::Layer::default()
-    .event_format(file_layer_format)
-    .with_writer(file_writer)
-    .json();
-  let subscriber =
-    tracing_subscriber::Registry::default()
-      .with(file_layer);
-  tracing::subscriber::set_global_default(subscriber)
-    .expect("unable to set global subscriber");
+  let logger_guard = Logger::new();
 
   // [x] let file_appender = tracing_appender::rolling::never(
   // [x]   log_root,
@@ -75,6 +91,29 @@ fn main() {
   alfa();
   bravo();
   println!("Process complete.");
+}
+
+fn set_up_logging(
+  log_dir: &PathBuf,
+  log_basename: &str,
+) {
+  let file_appender = tracing_appender::rolling::never(
+    log_dir,
+    log_basename,
+  );
+  let (file_writer, _guard) =
+    tracing_appender::non_blocking(file_appender);
+  let file_layer_format =
+    tracing_subscriber::fmt::format().json();
+  let file_layer = fmt::Layer::default()
+    .event_format(file_layer_format)
+    .with_writer(file_writer)
+    .json();
+  let subscriber =
+    tracing_subscriber::Registry::default()
+      .with(file_layer);
+  tracing::subscriber::set_global_default(subscriber)
+    .expect("unable to set global subscriber");
 }
 
 fn alfa() {
