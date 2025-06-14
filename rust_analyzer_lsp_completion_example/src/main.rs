7c3bb1a5a12a1a9cfbb354d1efe5d201dfc5d542
Alan Smith <commit@alanwsmith.com>
2025-06-14 12:48:23 -0400

    Move server capabilities to their own function

--- rust_analyzer_lsp_completion_example/src/main.rs
+++ rust_analyzer_lsp_completion_example/src/main.rs
@@ -7,6 +7,7 @@ use lsp_types::{
   GotoDefinitionResponse, InitializeParams,
   ServerCapabilities, request::GotoDefinition,
 };
+use serde_json::Value;
 use std::error::Error;
 use std::path::PathBuf;
 use tracing::{Level, event};
@@ -23,31 +24,44 @@ fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
   event!(Level::INFO, "Creating stdio transport");
   let (connection, io_threads) = Connection::stdio();
 
-  event!(Level::INFO, "Defining server capabilities");
-  let server_capabilities =
-    serde_json::to_value(&ServerCapabilities {
-      definition_provider: Some(OneOf::Left(true)),
-      ..Default::default()
-    })
-    .unwrap();
+  // let server_capabilities =
+  //   serde_json::to_value(&ServerCapabilities {
+  //     definition_provider: Some(OneOf::Left(true)),
+  //     ..Default::default()
+  //   })
+  //   .unwrap();
 
   event!(Level::INFO, "Waiting on initilization");
-  let initialization_params =
-    match connection.initialize(server_capabilities) {
-      Ok(it) => it,
-      Err(e) => {
-        if e.channel_is_disconnected() {
-          io_threads.join()?;
-        }
-        return Err(e.into());
+  let initialization_params = match connection
+    .initialize(server_capabilities())
+  {
+    Ok(it) => it,
+    Err(e) => {
+      if e.channel_is_disconnected() {
+        io_threads.join()?;
       }
-    };
+      return Err(e.into());
+    }
+  };
 
   main_loop(connection, initialization_params)?;
   io_threads.join()?;
 
   event!(Level::INFO, "Shutting down gracefully");
   Ok(())
+}
+
+fn server_capabilities() -> Value {
+  event!(Level::INFO, "Defining server capabilities");
+  serde_json::to_value(&ServerCapabilities {
+    completion_provider: Some(
+      lsp_types::CompletionOptions {
+        ..Default::default()
+      },
+    ),
+    ..Default::default()
+  })
+  .expect("Could not set up server capabilities")
 }
 
 fn main_loop(
@@ -60,17 +74,21 @@ fn main_loop(
   event!(Level::INFO, "Starting main loop");
 
   for msg in &connection.receiver {
-    eprintln!("got msg: {msg:?}");
+    event!(Level::INFO, "Got message: {msg:?}");
+
     match msg {
       Message::Request(req) => {
         if connection.handle_shutdown(&req)? {
           return Ok(());
         }
-        eprintln!("got request: {req:?}");
+
+        event!(Level::INFO, "Got request: {req:?}");
+
         match cast::<GotoDefinition>(req) {
           Ok((id, params)) => {
-            eprintln!(
-              "got gotoDefinition request #{id}: {params:?}"
+            event!(
+              Level::INFO,
+              "Request ID #{id} - Params {params:?}"
             );
             let result = Some(
               GotoDefinitionResponse::Array(Vec::new()),
@@ -92,13 +110,17 @@ fn main_loop(
           }
           Err(ExtractError::MethodMismatch(req)) => req,
         };
+
         // ...
       }
       Message::Response(resp) => {
-        eprintln!("got response: {resp:?}");
+        event!(Level::INFO, "Got response: {resp:?}");
       }
-      Message::Notification(not) => {
-        eprintln!("got notification: {not:?}");
+      Message::Notification(noti) => {
+        event!(
+          Level::INFO,
+          "Got notification: {noti:?}"
+        );
       }
     }
   }
@@ -122,22 +144,17 @@ pub fn init_logger(log_dir: &PathBuf) -> WorkerGuard {
     .max_log_files(2)
     .build(log_dir)
     .expect("Could not build tracing appender");
-
   let (writer, guard) =
     tracing_appender::non_blocking(appender);
-
   let layer = tracing_subscriber::fmt::Layer::default()
     .with_ansi(false)
     .with_writer(writer)
-    .pretty();
-
+    .compact();
   let subscriber =
     tracing_subscriber::Registry::default().with(layer);
-
   tracing::subscriber::set_global_default(subscriber)
     .expect(
       "Could not set tracing subscriber global default",
     );
-
   guard
 }
