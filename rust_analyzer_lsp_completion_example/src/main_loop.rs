42100abd1cc1bf10786818412c38a1dd2bf60338
Alan Smith <commit@alanwsmith.com>
2025-06-15 14:38:33 -0400

    Removed unused init params

--- rust_analyzer_lsp_completion_example/src/main_loop.rs
+++ rust_analyzer_lsp_completion_example/src/main_loop.rs
@@ -3,19 +3,15 @@ use crate::notifications::handle_notification;
 use crate::requests::handle_request;
 use crate::responses::handle_response;
 use lsp_server::{Connection, Message};
-use lsp_types::InitializeParams;
 use std::error::Error;
 use tracing::{Level, event};
 
 pub fn main_loop(
   connection: Connection,
-  params: serde_json::Value,
+  _params: serde_json::Value,
 ) -> Result<(), Box<dyn Error + Sync + Send>> {
   event!(Level::DEBUG, "Starting main loop");
   let mut global_state = GlobalState::new();
-
-  let _params: InitializeParams =
-    serde_json::from_value(params).unwrap();
 
   for msg in &connection.receiver {
     match msg {
