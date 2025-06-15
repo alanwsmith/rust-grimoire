c075b23e3fefedeb7e7e92942c468960e2fd38e3
Alan Smith <commit@alanwsmith.com>
2025-06-15 13:34:34 -0400

    cleanup

--- rust_analyzer_lsp_completion_example/src/server_capabilities.rs
+++ rust_analyzer_lsp_completion_example/src/server_capabilities.rs
@@ -1,32 +1,7 @@
-#![allow(unused)]
-use crate::mem_docs::{self, *};
-use lsp_server::{
-  Connection, ExtractError, Message, Request,
-  RequestId, Response,
-};
-use lsp_types::notification::{
-  DidChangeTextDocument, Notification,
-};
-use lsp_types::request::Completion;
-use lsp_types::{
-  CompletionItem, CompletionList, OneOf,
-  TextDocumentSyncKind,
-};
-use lsp_types::{
-  GotoDefinitionResponse, InitializeParams,
-  ServerCapabilities, request::GotoDefinition,
-};
-use serde::de::DeserializeOwned;
+use lsp_types::ServerCapabilities;
+use lsp_types::TextDocumentSyncKind;
 use serde_json::Value;
-use std::error::Error;
-use std::mem;
-use std::path::PathBuf;
 use tracing::{Level, event};
-use tracing_appender::non_blocking::WorkerGuard;
-use tracing_appender::rolling::{
-  RollingFileAppender, Rotation,
-};
-use tracing_subscriber::prelude::*;
 
 pub fn server_capabilities() -> Value {
   event!(Level::INFO, "Defining server capabilities");
