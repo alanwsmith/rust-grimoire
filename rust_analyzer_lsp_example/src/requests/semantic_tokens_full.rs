57ccb0a41293c97c3b54f09c190bd59ae7499bdd
Alan Smith <commit@alanwsmith.com>
2025-06-17 00:16:26 -0400

    Working on semantic token example

--- rust_analyzer_lsp_example/src/requests/semantic_tokens_full.rs
+++ rust_analyzer_lsp_example/src/requests/semantic_tokens_full.rs
@@ -1,16 +1,20 @@
+use std::num;
+
 use crate::global_state::GlobalState;
 use crate::requests::cast::cast_request;
 use lsp_server::{Request, Response};
 use lsp_types::{
-  CompletionItem, CompletionList, SemanticToken,
-  SemanticTokens, SemanticTokensResult,
+  SemanticToken, SemanticTokens, SemanticTokensResult,
   request::SemanticTokensFullRequest,
 };
+use serde::de;
+use serde_json::Value;
 use tracing::{Level, event};
+use unicode_segmentation::UnicodeSegmentation;
 
 pub fn semantic_tokens_full(
   message: Request,
-  _global_state: &GlobalState,
+  global_state: &GlobalState,
 ) -> Response {
   let id = message.id.clone();
   match cast_request::<SemanticTokensFullRequest>(
@@ -18,25 +22,17 @@ pub fn semantic_tokens_full(
   ) {
     Ok(params) => {
       event!(Level::TRACE, "{:?}", &params);
-
-      let response =
-        SemanticTokensResult::Tokens(SemanticTokens {
-          // TODO: Verify this is the right ID to send back
-          result_id: Some(id.to_string()),
-          data: vec![SemanticToken {
-            delta_line: 0,
-            delta_start: 1,
-            length: 4,
-            token_modifiers_bitset: 0,
-            token_type: 2,
-          }],
-        });
+      let uri = params.1.text_document.uri.to_string();
+      let initial_text =
+        &global_state.mem_docs.get(&uri).unwrap().data;
+      let highlights = highlight_tokens(
+        &initial_text,
+        &id.to_string(),
+      );
 
       Response {
         id,
-        result: Some(
-          serde_json::to_value(response).unwrap(),
-        ),
+        result: highlights,
         error: None,
       }
     }
@@ -52,29 +48,124 @@ pub fn semantic_tokens_full(
   }
 }
 
-// This is just a simple filter to provide
-// a working example. It sends back any of
-// the `word_list` that matches the current
-// input
-fn filter_words(input: &str) -> CompletionList {
-  let word_list = vec![
-    "-- test", "al", "alfa", "bravo", "braavo",
-    "charlie",
-  ];
+fn highlight_tokens(
+  input: &str,
+  id: &str,
+) -> Option<Value> {
+  // This is a very basic approach to finding
+  // text using windows for find the matches.
+  // It's meant to provide output for demonstration
+  // purposes. It's unlikely the approach is
+  // something you'd want to use in
+  // production code.
 
-  let filtered: Vec<_> = word_list
-    .iter()
-    .filter(|w| w.to_lowercase().starts_with(input))
-    .map(|w| {
-      CompletionItem::new_simple(
-        w.to_string(),
-        format!("This is the {}", w),
-      )
-    })
-    .collect();
+  let mut numbers = vec![];
+  let lines = input.lines();
+  for (line_index, line) in lines.enumerate() {
+    let characters =
+      line.graphemes(true).collect::<Vec<&str>>();
+    for (window_index, window) in
+      characters.windows(4).enumerate()
+    {
+      if window == &["a", "l", "f", "a"] {
+        let delta_line = line_index as u32;
+        let delta_start = window_index as u32;
 
-  CompletionList {
-    is_incomplete: filtered.len() > 0,
-    items: filtered,
+        numbers.push(delta_line);
+        numbers.push(delta_start);
+        numbers.push(4 as u32);
+        numbers.push(0 as u32);
+        numbers.push(2 as u32);
+
+        // let payload = vec![
+        //   delta_line,
+        //   delta_start,
+        //   4 as u32,
+        //   0 as u32,
+        //   2 as u32,
+        // ];
+        // numbers.push(payload);
+
+        // data.push(SemanticToken {
+        //   delta_line,
+        //   delta_start,
+        //   length: 4,
+        //   token_modifiers_bitset: 0,
+        //   token_type: 2,
+        // })
+      }
+    }
   }
+
+  for (i, w) in numbers
+    .windows(5)
+    .step_by(5)
+    .skip(1)
+    .rev()
+    .enumerate()
+  {
+    event!(
+      Level::ERROR,
+      "\n\n----------------------{:?} - {:?}\n\n--------------------",
+      i,
+      w
+    );
+  }
+
+  let data = vec![];
+
+  // let data: Vec<_> = numbers
+  //   .iter()
+  //   .rev()
+  //   .enumerate()
+  //   .map(|(i, v)| SemanticToken {
+  //     delta_line: 0,
+  //     delta_start: 1,
+  //     length: 4,
+  //     token_type: 2,
+  //     token_modifiers_bitset: 0,
+  //   })
+  //   .collect();
+
+  // {
+  //   v[0] = numbers[i + 1][0] - v[0];
+  //   // let check_index = numbers.len() - 1;
+  //   // event!(
+  //   //   Level::ERROR,
+  //   //   "check_index: {:?}",
+  //   //   check_index
+  //   // );
+  //   // if i < numbers.len() - 1 {
+  //   //   event!(
+  //   //     Level::ERROR,
+  //   //     "ccccccccccccccccccccccccccccccccccc"
+  //   //   );
+  //   //   // v[0] = numbers[i + 1][0] - v[0];
+  //   // }
+  //   event!(
+  //     Level::ERROR,
+  //     "\n\n----------------------{:?} - {:?}\n\n--------------------",
+  //     i,
+  //     v
+  //   );
+  // }
+
+  // for line_update in (5..numbers.len()).step_by(5) {
+  //   event!(
+  //     Level::ERROR,
+  //     "\n\n----------------------{:?}\n\n--------------------",
+  //     line_update
+  //   );
+  // }
+
+  Some(
+    serde_json::to_value(SemanticTokensResult::Tokens(
+      SemanticTokens {
+        // TODO: Verify this is the right ID to send back
+        result_id: Some(id.to_string()),
+        data,
+      },
+    ))
+    .unwrap(),
+  )
 }
